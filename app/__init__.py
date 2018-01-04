867db09edfecb56274a9abc22c2f1cb9e6ed9c57
Daniel Kuecker <daniel@kuecker.net>
2018-01-04 12:10:22 -0600

    Workaround for requests not working on GAE

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,12 @@ def create_app(config_name):
     #handler.setFormatter(formatter)
     #app.logger.addHandler(handler)
 
+    # Use the App Engine Requests adapter. This makes sure that Requests uses
+    # URLFetch.
+    if app.config['GAE']:
+        from google.appengine.ext import ndb
+        requests_toolbelt.adapters.appengine.monkeypatch()
+
     csrf.init_app(app)
 
     db.init_app(app)
@@ -70,3 +76,23 @@ def create_app(config_name):
         return render_template('errors/500.html', title='Server Error'), 500
 
     return app
+
+    if app.config['GAE']:
+        class Settings(ndb.Model):
+            name = ndb.StringProperty()
+            value = ndb.StringProperty()
+
+            @staticmethod
+            def get(name):
+                NOT_SET_VALUE = "NOT SET"
+                retval = Settings.query(Settings.name == name).get()
+                if not retval:
+                    retval = Settings()
+                    retval.name = name
+                    retval.value = NOT_SET_VALUE
+                    retval.put()
+                if retval.value == NOT_SET_VALUE:
+                    raise Exception(('Setting %s not found in the database. A placeholder record has been created.' +
+                       ' Go to the Developers Console for your app in App Engine, look up the Settings record with ' +
+                       'name=%s and enter its value in that records value field.') % (name, name))
+                return retval.value
